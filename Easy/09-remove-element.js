1669487b8bb1b648ce729d9874661572e7cf86bb
Jesse Smart <jsmartdev@protonmail.com>
2021-03-22 16:30:56 -0400

    solved remove element

---
+++ Easy/09-remove-element.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    for(i = 0; i < nums.length; i++) {
+      if(nums[i] === val) {
+         nums.splice(i, 1);
+         i--;
+      }
+    }
+    return nums.length;
+}; 

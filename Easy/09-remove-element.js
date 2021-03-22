14dbe678c651742cbdd368a8f123ad138b148477
Jesse Smart <jsmartdev@protonmail.com>
2021-03-22 16:34:28 -0400

    fix indentations in remove-element.js

--- Easy/09-remove-element.js
+++ Easy/09-remove-element.js
@@ -6,10 +6,10 @@
  */
 var removeElement = function(nums, val) {
     for(i = 0; i < nums.length; i++) {
-      if(nums[i] === val) {
-         nums.splice(i, 1);
-         i--;
-      }
+        if(nums[i] === val) {
+            nums.splice(i, 1);
+            i--;
+        }
     }
     return nums.length;
 }; 

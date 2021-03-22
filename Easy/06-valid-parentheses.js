1fdb08af00850320136674befa9d9cbaf0ca0d74
Jesse Smart <jsmartdev@protonmail.com>
2021-03-22 17:23:07 -0400

    solved valid parentheses

--- Easy/06-valid-parentheses.js
+++ Easy/06-valid-parentheses.js
@@ -4,5 +4,33 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+     // intialized an array called stack 
+    var stack = []; 
+    // creating an object called pairs that represents each character based on styled bracket
+    const pairs = {
+        '}':'{',
+        ']':'[',
+        ')':'(',
+    }
     
+  // the for...of statement creates a loop iterating over iterable objects
+    for (const v of strs) {
+    // created an a varible to track if brackets are open 
+    const open = pairs[v] 
+    // conditiontal that if a bracket is open 
+    if (open) {
+        console.log(open)
+        // if the stack removes the most recently added element is not equal to open return boolean
+        // !== (is not) compared two variables and but it ignores the datatype of variable.
+        if (stack.pop() !== open) {
+        // return boolean false 
+        return false
+        }
+    } else {
+        // creates the new element of the collection 
+        stack.push(v)
+        console.log(v)
+    }
+    }
+    return stack.length <= 0
 };
--- Easy/09-remove-element.js
+++ Easy/09-remove-element.js
@@ -4,6 +4,7 @@
  * @param {number} val
  * @return {number}
  */
+
 var removeElement = function(nums, val) {
     for(i = 0; i < nums.length; i++) {
         if(nums[i] === val) {

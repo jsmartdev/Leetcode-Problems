8ffee26fb944d6ca9539b3fcac3723fb570e7c07
Jesse Smart <jsmartdev@protonmail.com>
2021-03-24 13:40:37 -0400

    solved longest common prefix

--- Easy/05-longest-common-prefix.js
+++ Easy/05-longest-common-prefix.js
@@ -4,5 +4,18 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    
+    let prefix = ''
+    if (strs.length > 0) {
+    for (let i = 0; ; i++) {
+        const common = strs[0][i]
+        if (!common) { return prefix }
+        for (let j = 0; j < strs.length; j++) {
+            if (strs[j][i] !== common) {
+            return prefix;
+            }
+        }
+        prefix += common;
+    }
+    }
+    return prefix
 };
--- Easy/08-remove-duplicates-from-sorted-array.js
+++ Easy/08-remove-duplicates-from-sorted-array.js
@@ -9,9 +9,20 @@ var removeDuplicates = function(nums) {
     // creates a loop to iterate through the length of numbers
     for (let i = 0; i < nums.length; i++) {
         // if the index of numbers is (index -1) add one to len 
-        if(nums[i] !== nums[i-1]){
+        if(nums[i] !== nums[i-1]) {
             nums[len++] = nums[i]
 }
     }
- return len;
+    return len;
+};
+
+var removeDuplicates = function(nums) {
+    for (i = 0; i < nums.length; i++) {
+        // declare conditional statement - if current index is identical to next index
+        if (nums[i] == nums[i+1]) {
+            // remove current index from nums array
+            nums.splice(i, 1);
+            i--;
+        }
+    }
 };

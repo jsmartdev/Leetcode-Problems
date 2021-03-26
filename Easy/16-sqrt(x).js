feb24b3bb23cbaf83cd9f1b4d1f1c45fe6c2e056
Jesse Smart <jsmartdev@protonmail.com>
2021-03-26 15:17:33 -0400

    solved climbing stairs

--- Easy/16-sqrt(x).js
+++ Easy/16-sqrt(x).js
@@ -4,11 +4,14 @@
  * @return {number}
  */
 var mySqrt = function(x) {
+    // get square root of x
     sqrRoot = Math.sqrt(x);
+    // round number down to get rid of decimal point and numbers to the right of it
     answer = Math.floor(sqrRoot);
     return answer;
 };
 
+// more concise and efficient version of same methodology from above solution
 var mySqrt = function(x) {
     return Math.trunc(Math.sqrt(x));
 };
